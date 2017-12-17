2e23868b8cf1c695eab8523b8bde79a49fc09d39
dazhuhaspower <jiefenghaspower@gmail.com>
2017-12-17 01:06:27 -0800

    add HeroFormComponent part 1
    new routing to sign up
    import css from url
    redefine Hero
    basic forms
    two way binding
    diagnostic for debugging

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { InMemoryDataService } from './in-memory-data.service';
 import { MessagesComponent } from './messages/messages.component';
 import { MessageService } from './message.service';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { HeroFormComponent } from './hero-form/hero-form.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { HeroSearchComponent } from './hero-search/hero-search.component';
     HeroDetailComponent,
     MessagesComponent,
     DashboardComponent,
-    HeroSearchComponent
+    HeroSearchComponent,
+    HeroFormComponent
   ],
   imports: [
     AppRoutingModule,
--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,7 +1,11 @@
-/**
- * New typescript file
- */
 export class Hero {
-  id: number;
-  name: string;
+
+  constructor(
+    public id: number,
+    public name: string,
+    public power: string,
+    public alterEgo?: string
+    ) { }
+
 }
+
